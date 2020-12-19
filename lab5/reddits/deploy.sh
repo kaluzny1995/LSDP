67d1f60659ba73f271be9ad090dceb6ed5291fe1
kaluzny1995 <47966392+kaluzny1995@users.noreply.github.com>
2020-12-19 16:04:55 +0100

    Lab5 update
    Working plain Kubernetes manifests. Helm templates. Error with Postgres StatefulSet. Unable to port-forward.

---
+++ lab5/reddits/deploy.sh
@@ -0,0 +1,6 @@
+kubectl apply -f rabbitmq.yaml
+kubectl apply -f worker.yaml
+kubectl apply -f influxdb.yaml
+kubectl apply -f mongodb.yaml
+kubectl apply -f grafana.yaml
+kubectl apply -f redash.yaml
