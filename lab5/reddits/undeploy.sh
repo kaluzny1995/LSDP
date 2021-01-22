67b909dad2e9ec8590d531501370c1db11a7b63b
kaluzny1995 <47966392+kaluzny1995@users.noreply.github.com>
2021-01-22 02:32:36 +0100

    Plain manifests update
    Update of plain Kubernetes manifests. Last planned.

--- lab5/reddits/undeploy.sh
+++ lab5/reddits/undeploy.sh
@@ -2,5 +2,5 @@ kubectl delete deploy/redash-deploy sts/postgres-sts svc/postgres svc/redash-svc
 kubectl delete deploy/grafana-deploy svc/grafana-svc pvc/grafana-pvc pv/grafana-pv
 kubectl delete sts/mongodb-sts svc/mongodb-svc pvc/mongodb-pvc pv/mongodb-pv
 kubectl delete sts/influxdb-sts svc/influxdb-svc pvc/influxdb-pvc pv/influxdb-pv
-kubectl delete deploy/worker-deploy
+kubectl delete deploy/worker-deploy cm/worker-cm
 kubectl delete sts/rabbitmq-sts svc/rabbitmq-svc
